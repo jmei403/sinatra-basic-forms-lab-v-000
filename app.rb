f682165f76168745d10bf219016ddf5a19dda4b2
Jason Mei <jason.mei403@gmail.com>
2019-01-19 18:59:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,17 +3,12 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    # @puppies = Puppy.all
     erb :index
   end
 
   get '/new' do
     erb :create_puppy
   end
-
-  # get '/puppy/:id' do
-  #   @puppy = Puppy.find(params[:id])
-  # end
 
   post '/puppy' do
     @puppy = Puppy.new(params[:name], params[:breed], params[:age])
