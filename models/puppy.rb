ca29c0253ad0ac273cfd6b94c76f7441a477be24
Jason Mei <jason.mei403@gmail.com>
2019-01-19 19:14:52 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,16 +1,16 @@
 class Puppy
   attr_accessor :name, :breed, :age
 
-  # @@all = []
+  @@all = []
 
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
-    # @@all << self
+    @@all << self
   end
 
-  # def self.all
-  #   @@all
-  # end
+  def self.all
+    @@all
+  end
 end
