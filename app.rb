1d33e8527c7587b81fbc560a6c846e2ad1a34984
Jason Mei <jason.mei403@gmail.com>
2019-01-19 18:18:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,8 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-  post '/puppy' do 
+  post '/puppy' do
+    @puppy_info = Puppy.new(params[:name], params[:breed], params[:age])
     erb :display_puppy
   end
 end
